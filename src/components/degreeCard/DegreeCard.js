585f2254cdcd65bcfdcafee163cdda03eb3a519a
drslaughterrex <drslaughterrex@gmail.com>
2021-09-11 11:34:38 -0500

    Final

--- src/components/degreeCard/DegreeCard.js
+++ src/components/degreeCard/DegreeCard.js
@@ -75,8 +75,7 @@ function DegreeCard(props) {
             style={{
               maxWidth: "100%",
               maxHeight: "100%",
-              transform: "scale(-50%, -50%)",
-              transform: "rotate(0deg)",
+              transform: "scale(-50%, -50%), rotate(0deg)",
             }}
             src={require(`../../assests/images/${degree.logo_path}`)}
             alt={degree.alt_name}
